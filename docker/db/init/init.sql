2773aecbe2b01e3112ebe54bb0de6d80a04ff36c
karinaaa-ed <karinaedigaryan265@gmail.com>
2026-02-14 13:15:02 +0300

    Add database and films table init script

--- docker/db/init/init.sql
+++ docker/db/init/init.sql
@@ -1,1 +1,12 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+SELECT 'CREATE DATABASE review2'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'review2')\gexec
+
+\connect review2
+
+CREATE TABLE IF NOT EXISTS films (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    url TEXT,
+    summary TEXT,
+    created_at TIMESTAMPTZ DEFAULT NOW()
+);
